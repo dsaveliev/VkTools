068622f2130126ddd547cd741e0c9cb81b484ea2
m.malkin <mmalkin@fun-box.ru>
2012-03-29 18:15:00 +0400

    непонятная ошибка в методе get_cookies временно исправлена костылём (#USSD-460)

--- lib/vk_tools/auth.rb
+++ lib/vk_tools/auth.rb
@@ -142,9 +142,13 @@ module VkTools::Auth
     def get_cookies(agent, login, password)
       page = agent.get("http://m.vk.com/login")
       form = page.forms.first
-      form.email = login
-      form.pass = password
-      page = agent.submit(form, form.buttons.first)
+
+      if form && form.has_field?(:email)
+        form.email = login
+        form.pass = password
+        agent.submit(form, form.buttons.first)
+      end
+
       cookie_jar = agent.cookie_jar
       cookie = { :cookie => cookie_jar.dump_cookies }
     end
